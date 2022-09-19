ea3f35fa1f12c706bde874c87098e9591fb3e587
fabio <fabiojuvenalpereira@hotmail.com>
2022-09-19 20:58:06 -0300

    refactor: change middleware error name

--- src/middlewares/error.midleware.ts
+++ src/middlewares/error.midleware.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 
-export default function error(err: any, _req: Request, res:Response) {
+export default function errorMiddleware(err: any, _req: Request, res:Response) {
   if (err.status) return res.status(err.status).json({ message: err.message });
 
   return res.status(500).json({ message: 'Internal Server Error' });
