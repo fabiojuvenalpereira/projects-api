7d830773a6d6dc4bc9ce809a97dc85b396c77171
fabio <fabiojuvenalpereira@hotmail.com>
2022-09-21 14:10:37 -0300

    feat: add the router to application

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,16 @@
 import { Router } from 'express';
+import insertProject from '../controllers/projects.cotroller';
 
 const router = Router();
+
+router.post('/', insertProject);
+
+router.get('/', insertProject);
+router.get('/:id', insertProject);
+
+router.put('/', insertProject);
+
+router.delete('/', insertProject);
+router.delete('/:id', insertProject);
 
 export default router;
