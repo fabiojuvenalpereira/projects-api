b15f062e959db2f7a132633bc2cf47e056aaa391
fabio <fabiojuvenalpereira@hotmail.com>
2022-09-22 19:11:43 -0300

    fix: add validation to objectId

--- src/services/projects.service.ts
+++ src/services/projects.service.ts
@@ -1,31 +1,38 @@
-import * as ProjectModel from '../models/projects.model';
+import * as projectsModel from '../models/projects.model';
+import ObjectIdValidate from './utils/objectIdValidate';
 
 export async function insertNewProject(project: any) {
-  const insertedProject = await ProjectModel.insertProjectOnDataBase(project);
+  const insertedProject = await projectsModel.insertProjectOnDataBase(project);
   return { status: 201, content: insertedProject };
 }
 
 export async function listProjects() {
-  const foundProjects = await ProjectModel.listProjectsFromDataBase();
+  const foundProjects = await projectsModel.listProjectsFromDataBase();
   return { status: 200, content: foundProjects };
 }
 
-export async function findProject(projectId: any) {
-  const foundProject = await ProjectModel.findProjectOnDataBase(projectId);
+export async function findProject(id: string) {
+  const projectId = ObjectIdValidate(id);
+
+  const foundProject = await projectsModel.findProjectOnDataBase(projectId);
   return { status: 200, content: foundProject };
 }
 
-export async function updateProject(data:any) {
-  const insertedProject = await ProjectModel.updateProjectOnDataBase(project, data);
+export async function updateProject(id:string, data:any) {
+  const projectId = ObjectIdValidate(id);
+
+  const insertedProject = await projectsModel.updateProjectOnDataBase(projectId, data);
   return { status: 200, content: insertedProject };
 }
 
-export async function deleleProject(projectId:any) {
-  const deleteProject = await ProjectModel.deleteProjectOnDataBase(projectId);
+export async function deleleProject(id:string) {
+  const projectId = ObjectIdValidate(id);
+
+  const deleteProject = await projectsModel.deleteProjectOnDataBase(projectId);
   return { status: 200, content: deleteProject };
 }
 
 export async function deleleAllProjects() {
-  const deletedProjects = await ProjectModel.deleteAllProjectOnDataBase();
+  const deletedProjects = await projectsModel.deleteAllProjectOnDataBase();
   return { status: 200, content: deletedProjects };
 }
