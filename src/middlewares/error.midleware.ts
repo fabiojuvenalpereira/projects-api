afb2256edb8f9a0de9e6d6159c22c65727261bf4
fabio <fabiojuvenalpereira@hotmail.com>
2022-10-03 19:55:01 -0300

    refactor: change hard coded status to referenced status

--- src/middlewares/error.midleware.ts
+++ src/middlewares/error.midleware.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
 import { RequestError } from '../interfaces/requestError.interface';
+import STATUS from '../utils/httpStatusCode';
 
 const errorMiddleware = (err: RequestError, _req: Request, res: Response, _next: NextFunction) => {
-  const status = err.status || 500;
+  const status = err.status || STATUS.INTERNALERROR;
   const message = err.message || 'Internal Server Error';
 
-  return res.status(status).json(message);
+  return res.status(status).json({ message });
 };
 
 export default errorMiddleware;
