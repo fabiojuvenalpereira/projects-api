06c268546396ab9b8d9791fd16f30c1cb968375f
fabio <fabiojuvenalpereira@hotmail.com>
2022-10-04 18:34:58 -0300

    refactor: change pattern to validate

--- src/utils/validateEntries.ts
+++ src/utils/validateEntries.ts
@@ -1,25 +1,24 @@
-import { ProjectInterface } from "../interfaces/project.interface";
 import Joi from 'joi';
 
-import NewError from "./errorGenerate";
-import STATUS from "./httpStatusCode";
+import { ProjectInterface } from '../interfaces/project.interface';
+import GenetateError from './errorGenerate';
+import STATUS from './httpStatusCode';
 
 const SCHEMA = Joi.object({
   title: Joi.string(),
   abstractContent: Joi.string(),
   content: Joi.string(),
   images: Joi.array().items(Joi.string()),
-  linkGitHub: Joi.string().pattern(new RegExp(/(fabiojuvenalpereira)/)),
+  linkGitHub: Joi.string().pattern(/(fabiojuvenalpereira)/).message('pattern is not valid'),
   linkProject: Joi.string(),
-  skills: Joi.array().items(Joi.string().alphanum().max(15)),
+  skills: Joi.array().items(Joi.string()),
 });
 
-
 const validateEntries = (data: ProjectInterface) => {
-  const validation = SCHEMA.validate({...data});
+  const validation = SCHEMA.validate({ ...data });
   if (validation.error) {
-    throw NewError(STATUS.BADREQUEST, validation.error.message)
+    throw new GenetateError(STATUS.BADREQUEST, validation.error.message);
   }
-}
+};
 
 export default validateEntries;
