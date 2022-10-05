22a3b547261ece31edb69f65fb75b38facdb334d
fabio <fabiojuvenalpereira@hotmail.com>
2022-10-05 14:21:14 -0300

    feat: add the new interface to update projects

---
+++ src/interfaces/projectUpdate.interface.ts
@@ -0,0 +1,9 @@
+export interface ProjectUpdateInterface {
+  title?: string,
+  abstractContent?: string,
+  content?: string,
+  images?: string[],
+  linkGitHub?: string,
+  linkProject?: string,
+  skills?: string[],
+}
