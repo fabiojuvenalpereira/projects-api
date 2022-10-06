b38f824a28ffab420b48738cd00d5cf51fbdb64c
fabio <fabiojuvenalpereira@hotmail.com>
2022-10-06 15:45:14 -0300

    refactor: remove projectUpdate interface

--- src/interfaces/projectUpdate.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface ProjectUpdateInterface {
-  title?: string,
-  abstractContent?: string,
-  content?: string,
-  images?: string[],
-  linkGitHub?: string,
-  linkProject?: string,
-  skills?: string[],
-}
