b7e308ee8d1f95063d6a313b6155c545c2efe258
fabio <fabiojuvenalpereira@hotmail.com>
2022-10-06 15:55:29 -0300

    feat: add hash validation middleware to authorizate user input

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express';
+import validateHash from '../auth/validateHash';
 import * as projectsController from '../controllers/projects.cotroller';
 
 const router = Router();
 
-router.post('/', projectsController.insertProject);
+router.post('/', validateHash, projectsController.insertProject);
 
-router.get('/', projectsController.listProjects);
-router.get('/:id', projectsController.findProject);
+router.get('/', validateHash, projectsController.listProjects);
+router.get('/:id', validateHash, projectsController.findProject);
 
-router.put('/:id', projectsController.updateProject);
+router.put('/:id', validateHash, projectsController.updateProject);
 
-router.delete('/', projectsController.deleleAllProjects);
-router.delete('/:id', projectsController.deleteProject);
+router.delete('/', validateHash, projectsController.deleleAllProjects);
+router.delete('/:id', validateHash, projectsController.deleteProject);
 
 export default router;
