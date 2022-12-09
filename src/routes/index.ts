b0b3e939d1825b2b46912896aded3f2e650dfc7b
fabio <fabiojuvenalpereira@hotmail.com>
2022-12-08 22:53:48 -0300

    fix: add new route to the api

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,9 @@
 import { Router } from 'express';
-import validateHash from '../auth/validateHash';
-import * as projectsController from '../controllers/projects.cotroller';
+
+import projectsRouter from './projects/index';
 
 const router = Router();
 
-router.post('/', validateHash, projectsController.insertProject);
-
-router.get('/', projectsController.listProjects);
-router.get('/:id', projectsController.findProject);
-
-router.put('/:id', validateHash, projectsController.updateProject);
-
-router.delete('/', validateHash, projectsController.deleleAllProjects);
-router.delete('/:id', validateHash, projectsController.deleteProject);
+router.use('/projects', projectsRouter);
 
 export default router;
